c73f30a93417372dfb29a832b969bc8c8c2e1c52
monshein <ms.myatmonshein@gmail.com>
2018-07-15 23:06:17 +0630

    start to create geo picker

---
+++ app/src/main/java/geo/places/com/mon/geopicker/MainActivity.java
@@ -0,0 +1,13 @@
+package geo.places.com.mon.geopicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
